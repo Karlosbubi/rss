0763f52d0ca6e93c0b314b9d59ef13adc8e2b72e
Kurt Brunner <Kontakt@Karlosbubi.de>
2022-11-06 13:38:30 +0100

    icen blank screen (no errors)

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@
 use std::error::Error;
 use rss::{Channel};
 use tokio;
-use iced::{Settings, Length};
-use iced::pure::widget::{Button, Column, Container, Text};
-use iced::pure::Sandbox;
+use iced::{Settings, Length, Scrollable, scrollable};
+use iced::widget::{Button, Column, Container, Text};
+use iced::Sandbox;
 
 
 #[derive(Debug)]
@@ -17,10 +17,12 @@ struct Post {
 #[derive(Debug)]
 struct Reader {
     url : String,
-    posts : Vec<Post>
+    posts : Vec<Post>,
+
+    scrollable_state: scrollable::State,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Messages {
     Refresh
 }
@@ -29,8 +31,9 @@ enum Messages {
 fn main() {
     println!("Hello, world!");
     let mut rss = Reader::new("https://www.bundesgesundheitsministerium.de/meldungen.xml".to_string()).expect("Error");
-    rss.update();
+    rss.fetch();
     rss.log();
+    println!("{:?}",Reader::run(Settings::default()));
 }
 
 impl Reader {
@@ -38,13 +41,14 @@ impl Reader {
         Ok(Reader{
             url,
             posts : Vec::new(),
+            scrollable_state: scrollable::State::new()
         })
     }
-    pub fn update(&mut self){
+    pub fn fetch(&mut self){
         let rt = tokio::runtime::Runtime::new().unwrap();
         let feed = rt.block_on(example_feed(self.url.to_string())).unwrap();
         //let feed = futures::executor::block_on(example_feed(self.url.to_string())).unwrap();
-        self.posts = feed.items().iter().map(|p| 
+        self.posts = feed.items().iter().take(20).map(|p| 
             Post{
                 title : p.title().unwrap_or("No Title Provided").to_string(),
                 description : p.description().unwrap_or("No Description Provided").to_string(),
@@ -73,7 +77,7 @@ impl Sandbox for Reader {
 
     fn update(&mut self, message: Self::Message) {
         match message {
-            Messages::Refresh => self.update(),
+            Messages::Refresh => self.fetch(),
         }
     }
 
@@ -81,16 +85,36 @@ impl Sandbox for Reader {
         Reader{
             url : "https://www.bundesgesundheitsministerium.de/meldungen.xml".to_string(),
             posts : Vec::new(),
+            scrollable_state : scrollable::State::new()
         }
     }
 
-    fn view(&self) -> iced::pure::Element<'_, Self::Message> {
-        Container::new(Text::new("Hello Iced"))
-            .width(Length::Fill)
+    fn view(&mut self) -> iced::Element<'_, Self::Message> {
+        //let post = Post{title : "Title".to_string(), description: "Description".to_string(), url : "www.url.com".to_string()};
+
+        let mut news = Scrollable::new(&mut self.scrollable_state)
+            .spacing(20)
+            .padding(25);
+
+        for post in &mut self.posts {
+            news = news.push(post.view());
+        }
+
+        Container::new(news)
+            .width(Length::FillPortion(2))
             .height(Length::Fill)
-            .center_x()
-            .center_y()
             .into()
+    }
+}
+
+impl Post {
+    pub fn view(&self) -> iced::Element<Messages> {
+        Column::new()
+        .push(Text::new(&self.title).size(20))
+        .push(Text::new(&self.description).size(12))
+        .push(Text::new(&self.url))
+        .spacing(8)
+        .into()
     }
 }
 
